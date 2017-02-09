887da9cb3c0be348299babba876b57aff4933a89
Santhosh Ramaiah <santhosh.r411@gmail.com>
2017-02-09 13:22:31 -0500

    add support for orientation change

--- CardView.swift
+++ CardView.swift
@@ -1,6 +1,6 @@
 //
 //  CardView.swift
-//  
+//  buenobonitoybarato
 //
 //  Created by santhosh ramaiah on 1/31/17.
 //  Copyright © 2017 santhosh ramaiah. All rights reserved.
@@ -9,12 +9,12 @@
 import Foundation
 
 import UIKit
-import SnapKit // no need to use snap kit. you can regular auto layout syntax
+import SnapKit
 
 class CardView: UIView {
     var totalHeight: CGFloat = 0
     var totalWidth: CGFloat = 0
-    var stackView: UIStackView // for stacking multiple views, 
+    var stackView: UIStackView
     
     init(width: CGFloat){
         totalWidth = width
@@ -29,12 +29,12 @@ class CardView: UIView {
         super.init(frame: CGRect(x: 0, y:  0, width: width, height: 0))
         super.addSubview(stackView)
         
-        
-        //insert auto layout code
         stackView.snp.makeConstraints { (make) -> Void in
             make.centerX.equalToSuperview()
             make.top.equalToSuperview()
             make.bottom.equalToSuperview()
+            make.right.equalToSuperview()
+            make.left.equalToSuperview()
         }
         
     }
@@ -59,7 +59,6 @@ class CardView: UIView {
         totalHeight = 0
     }
     
-    // no need incase if you need more height between views
     public func prepareBlankView(height: CGFloat){
         let blankView = UIView(frame: CGRect(x: 0, y:  0, width: totalWidth - 40, height: height))
         blankView.heightAnchor.constraint(equalToConstant: height).isActive = true
@@ -68,16 +67,23 @@ class CardView: UIView {
     
     public func showCardShadow(){
         self.backgroundColor = UIColor.white
-        self.frame = CGRect(x: 0, y: 0, width: totalWidth, height: totalHeight - 8)
+        //self.frame = CGRect(x: 0, y: 0, width: totalWidth, height: totalHeight - 8)
         self.layer.masksToBounds = false
         self.layer.shadowOffset = CGSize(width: 0, height: 0.0);
         self.layer.cornerRadius = 0;
         self.layer.shadowRadius = 2;
         self.layer.shadowOpacity = 0.5;
+        //self.layer.shouldRasterize = true
         self.shadowPath = UIBezierPath(rect: self.bounds).cgPath
     }
     
-        
+    
+    
+    override func layoutSubviews() {
+        showCardShadow()
+        super.layoutSubviews()
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
